e6ad4127a0e70ca4b77dfd6ac9e7489f17803545
currysaucee <jonathanau1999@gmail.com>
2022-03-29 12:04:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello Glasgow!")
+print("==========================================================")
+
+x = int(input("Please input for x:"))
+y = int(input("Please input for y:"))
+print("your average is:", int((x+y)/2))
+print("==========================================================")
+
+
+mod_name = input("check what module you want\n")
+
+if (mod_name) == ("CSC1009"): print("Object Oriented Programming")
+else: print("Unkown module")
+
+print("==========================================================")
+for index in range(101,66,-2): print(index)
+print("==========================================================")
